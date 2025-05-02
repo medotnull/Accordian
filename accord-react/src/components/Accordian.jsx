4fe3d9e06981c9f2dc070c3441ca028e53c5597d
medotnull <meenalbansodbmw@gmail.com>
2025-05-03 00:00:44 +0530

    Iniitial commit

---
+++ accord-react/src/components/Accordian.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {FAQ} from '../API/faq.json'
+
+const Accordian = () => {
+    const [data, setData] = 
+  return (
+    <div>Accordian</div>
+  )
+}
+
+export default Accordian
