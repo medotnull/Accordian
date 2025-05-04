974288f2de5c24900ada71a3c38bd64ef06c0fa3
medotnull <meenalbansodbmw@gmail.com>
2025-05-04 23:59:32 +0530

    /

--- accord-react/src/components/Accordian.jsx
+++ accord-react/src/components/Accordian.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import {FAQ} from '../API/faq.json'
+import React, { useEffect, useState } from 'react'
+import faq from '../API/faq.json'
+import { useState } from 'react';
 
 const Accordian = () => {
-    const [data, setData] = 
-  return (
-    <div>Accordian</div>
-  )
-}
+    const [data, setData] = useState([]);
+    useEffect(() => {
+      setData(faq);
+    }, []);
+  return <>
+  <h1>The Accordion</h1>
+  <ul className='section-accordion'>
+    {
+      data.map((curElem) => {
+        return <li key={curElem.id}>
+          <div className='accoridion-grid'>
+            <p>{curElem.question}</p>
+            <button>Show</button>
+          </div>
+        </li>
+      })
+    }
+  </ul>
+  </> 
+  
+};
 
 export default Accordian
